755ee7595915d3caa399c096cc6b7b8a608e3579
claudiahalip <claudia4ro@yahoo.com>
2020-12-11 15:58:07 -0600

    user actions

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import photographerReducer from './reducers/photographerReducer'
 import { BrowserRouter as Router,} from "react-router-dom";
+import userReducer from './reducers/userReducer';
 
+
+const reducer = combineReducers({
+  photographerReducer, userReducer
+})
 const store = createStore(photographerReducer, composeWithDevTools(applyMiddleware(thunk)));
 
+//const store = createStore(reducer, applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
    <Provider store={store}>
--- src/reducers/photographerReducer.js
+++ src/reducers/photographerReducer.js
@@ -13,8 +13,21 @@ export default function photographerReducer(
                 return {...state, loading: true}
             case 'ADD_REVIEW':
                 return {...state, reviews: [...state.reviews, action.payload]}
+
+                //user reducer
+
+            case "GOT_USERS":
+                return{...state, loading: false, users: action.payload}
+            case "FETCHING_USER":
+                return {...state, loading:true}
+            case "ADD_USER":
+                return {...state, users: [...state.users, action.payload]}
+                    
             default:
                 return state;
         }
+
+
+        //user actions
 
 }
